413c37383c53a36b52cba5d0712bc3cf5ad6e8dc
chahak.shah.cs <chahak.shah.cs@gmail.com>
2025-08-07 11:44:54 +0000

    initial commit

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,6 +6,7 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
      pkgs.gcc
+     pkgs.fmt
       pkgs.clang
       pkgs.clang-tools
       pkgs.gdb
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <fmt/core.h>
+#include "app/hello.hpp"
 
 int main() {
     fmt::print("Using fmt with Conan 🎉\n");
+    std::string greetMessage =  greet("Chahak");
+    fmt::print(greetMessage);
+
     return 0;
 }
